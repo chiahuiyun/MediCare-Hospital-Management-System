860f33a2930fd58b2b5f9baf3893c690d9d30a67
chiahuiyun <80665061+chiahuiyun@users.noreply.github.com>
2026-02-14 21:26:16 +0800

    Initial commit

---
+++ admin/src/pages/Appointments.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import AppointmentsPage from '../components/AppointmentsPage'
+
+
+const Appointments = () => {
+  return (
+    <div>
+        <Navbar />
+        <AppointmentsPage />
+    </div>
+  )
+}
+
+export default Appointments
