d68c97e8dff41bcbef212bc223cc29a6644a24d1
chiahuiyun <80665061+chiahuiyun@users.noreply.github.com>
2026-02-15 19:28:20 +0800

    Update api links

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,8 @@ const app = express();
 const port = 4000;
 
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174"
+  process.env.FRONTEND_URL,
+  process.env.ADMIN_URL
 ]
 
 // Middleware
